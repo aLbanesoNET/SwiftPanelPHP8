b12d3b825c3c5ae8e4fd5e6670e9b839536f3919
aLbanesoNET <info@albaneso.net>
2026-09-04 21:56:30 -0400

    Remove abandoned Vertex theme

--- admin/templates/vertex/footer.php
+++
@@ -1,2 +0,0 @@
-<?php if (!defined('SITENAME')) { http_response_code(403); exit('Forbidden'); } ?>
-<?php if($page!=='login'): ?></div></main><footer class="av-footer">Swift Panel administration <span><?= defined('VERSION')?htmlspecialchars(VERSION):'PHP 8' ?></span></footer></div><?php else: ?></div></main><?php endif; ?></body></html>
